7f9593afedf4e1fc010468c19abc3762472e3b03
Vladimir Agafonkin <agafonkin@gmail.com>
2015-11-12 13:04:18 +0200

    clean up gh-pages

--- demo/index.js
+++ demo/index.js
@@ -1,2 +1,1 @@
-
-// code
+console.log(hello());
--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var hello = require('../'),
-    t = require('tap').test;
-
-t('returns Hello World', function (t) {
-    t.equal(hello(), 'Hello World');
-    t.end();
-});
